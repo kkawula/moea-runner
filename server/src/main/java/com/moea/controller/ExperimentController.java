d71d722505daf0decb50ac987cfe0e41498a9d53
Jakub Konopka <jakubkonopka@student.agh.edu.pl>
2024-12-10 22:02:33 +0100

    Add missing imports

--- server/src/main/java/com/moea/controller/ExperimentController.java
+++ server/src/main/java/com/moea/controller/ExperimentController.java
@@ -2,13 +2,16 @@ package com.moea.controller;
 
 import com.moea.ExperimentStatus;
 import com.moea.dto.ExperimentDTO;
+import com.moea.exceptions.ExperimentNotFoundException;
 import com.moea.model.Experiment;
 import com.moea.model.ExperimentMetricResult;
 import com.moea.service.ExperimentService;
 
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import org.moeaframework.analysis.collector.Observations;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,6 +46,7 @@ public class ExperimentController {
 
     @PostMapping()
     public Long createExperiment(@RequestBody ExperimentDTO experimentDTO) {
+        // TODO: Add name checking for algorithms, problem and metrics to throw BAD_REQUEST
         Long newExperimentID = experimentService.saveNewRunningExperiment(experimentDTO);
         List<Observations> results = new ArrayList<>();
 
@@ -51,7 +55,7 @@ public class ExperimentController {
                 .observeOn(Schedulers.io())
                 .doOnComplete(() -> System.out.println("END OF EXPERIMENT: " + newExperimentID))
                 .doOnComplete(() -> experimentService.saveExperimentResults(newExperimentID, results))
-                .doOnError(e -> experimentService.updateExperimentStatus(newExperimentID, ExperimentStatus.ERROR))
+                .doOnError(_ -> experimentService.updateExperimentStatus(newExperimentID, ExperimentStatus.ERROR))
                 .subscribe();
 
         return newExperimentID;

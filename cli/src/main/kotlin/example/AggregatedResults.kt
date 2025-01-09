c15557f71d457e6f39550072d70f2e8827eeb795
kkawula <57270771+kkawula@users.noreply.github.com>
2025-01-09 19:04:15 +0100

    Add date filtering

--- cli/src/main/kotlin/example/AggregatedResults.kt
+++ cli/src/main/kotlin/example/AggregatedResults.kt
@@ -8,7 +8,8 @@ fun main() = runBlocking {
     println("Getting aggregated results...")
 
     val apiClient = ApiClient()
-    val aggregatedResults = apiClient.getAggregatedExperimentsResults(listOf(1, 2))
+    val aggregatedResults =
+        apiClient.getAggregatedExperimentsResults(listOf(1, 2), "1410-01-01 11:59:59", "2077-12-31 11:59:59")
 
     printFormattedAggregatedResults(aggregatedResults)
 
